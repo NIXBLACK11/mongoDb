ed494bdf4a5d8d8e4678f396305c1664c4d8c92e
Siddharth Singh Rana <91743459+NIXBLACK11@users.noreply.github.com>
2023-05-02 19:26:15 +0530

    Update insertMongo.py

--- insertMongo.py
+++ insertMongo.py
@@ -3,7 +3,7 @@ import pymongo
 if __name__ == "__main__":
     print("Welcome to pyMongo")
     # connect to mongoDb
-    client = pymongo.MongoClient("mongodb+srv://NIXBLACK:nixblack11@cluster0.tk5azpj.mongodb.net/test")
+    client = pymongo.MongoClient("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
     # to check if connected to mongoDb
     # print(client)
     # to make a database
